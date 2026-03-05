fe57ee47052aa2e3d691110928bd1e80ae679dd8
adrita06 <adritaahsan06@gmail.com>
2026-03-05 15:33:58 +0600

    initial

---
+++ kern/thread/PTQueueInit/export.h
@@ -0,0 +1,17 @@
+#ifndef _KERN_THREAD_PTQUEUEINIT_H_
+#define _KERN_THREAD_PTQUEUEINIT_H_
+
+#ifdef _KERN_
+
+void tqueue_init(unsigned int mbi_addr);
+void tqueue_enqueue(unsigned int chid, unsigned int pid);
+unsigned int tqueue_dequeue(unsigned int chid);
+void tqueue_remove(unsigned int chid, unsigned int pid);
+
+void ready_enqueue(unsigned int tid, unsigned int priority);
+unsigned int ready_dequeue(void);
+
+#endif /* _KERN_ */
+
+#endif /* !_KERN_THREAD_PTQUEUEINIT_H_ */
+
